e9b8962f7b72c9e97775803fdf31e0a495987f59
Nischal Boj <nischalboj@gmail.com>
2026-02-14 15:59:32 +0545

    first commit

--- firstCode/script1.js
+++ firstCode/script1.js
@@ -1,3 +1,5 @@
 console.log("object");
 const fs = require('fs')
 console.log(fs.readFileSync('sample.txt').toString());
+const ab = require('fs')
+console.log("this is the first node js code we have written although i understand this code have done this on another language of the ");
